4542fe1e0860debe0bfb62d634b4afcb149eb980
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-07-23 02:10:45 -0300

    Foobar

--- Monitorar Tropas Chegando/content.js
+++ Monitorar Tropas Chegando/content.js
@@ -1,1 +1,10 @@
-$.getScript( 'https://victorserpac.github.io/Tribal-Wars/monitorar_ataques_chegando.js' );
+(function(d, script) {
+    script = d.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function(){
+        // remote script has loaded
+    };
+    script.src = 'https://victorserpac.github.io/Tribal-Wars/monitorar_ataques_chegando.js';
+    d.getElementsByTagName('head')[0].appendChild(script);
+}(document));
