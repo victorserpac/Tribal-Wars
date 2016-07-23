18fc4cc13c256bc7babb39bc7386e91ee40a583b
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-07-23 02:24:47 -0300

    Foobar

--- monitorar_ataques_chegando.js
+++ monitorar_ataques_chegando.js
@@ -3,9 +3,5 @@ var numAttackElement = document.querySelector( '#incomings_amount' );
 console.log( numAttackElement );
 
 numAttackElement.addEventListener( 'DOMSubtreeModified', function( event ) {
-    console.log('foo');
-});
-
-numAttackElement.addEventListener( 'DOMCharacterDataModified', function( event ) {
-    console.log('bar');
+    location.reload();
 });
